b46353e4b44a7ecbd321d1c25e4530b56c936cab
Dragos Nedelcu <dragosgn@gmail.com>
2019-07-08 11:49:55 +0200

    arrow funcitons

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,37 @@ console.log(v);
 // console.log(l), l is not defined (let declarations live inside of the block scope)
 
 // diff between "===" and "=="
+// == only compares value, === compares value and type
+
+if ("1" == 1) {
+  console.log("I am not looking at the type.");
+}
+
+if ("1" === 1) {
+  console.log("I am not looking at the type.");
+} else {
+  console.log("I am looking at the type");
+}
+
+// difference between let and const
+// difference between "undefined" and "null"
+// they both define and empty value, but undefined gets assigned automaticaly
+// also type of undefined is undefined and typeof null is object
+
+// use of arrow function
+const person = {
+  firstName: "",
+  secondName: "",
+  setName: function(name) {
+    let splitName = n => {
+      let nameArray = n.split(" ");
+      this.firstName = nameArray[0];
+      this.secondName = nameArray[1];
+    };
+
+    splitName(name);
+  }
+};
+
+person.setName("John Connor");
+console.log("firstname", person.firstName);
